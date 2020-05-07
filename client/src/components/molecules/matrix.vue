228eed76c8b998375d7291c378b09c92f098f7aa
FelixIsaac <limfelix16@gmail.com>
2020-05-07 21:56:29 +0800

    Added bar

---
+++ client/src/components/molecules/matrix.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <template>
+      <span v-show="measurement === 'wps'">
+        WPS: {{ wordsTyped.correct / seconds }}
+      </span>
+      <span v-show="measurement === 'wpm'">
+        WPM: {{ wordsTyped.correct / (seconds / 60) }}
+      </span>
+    </template>
+    <span>/ ACC: {{ (wordsTyped.incorrect / wordsTyped.total) * 100 }}%</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "matrix",
+    props: ['measurement', 'wordsTyped', 'seconds']
+  }
+</script>
---
+++ client/src/components/organisms/bar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bar">
+    <quick-settings v-bind:mode="settings.mode"/>
+    <matrix measurement="settings.measurement" words-typed="result.wordsTyped" seconds="result.seconds"/>
+  </div>
+</template>
+
+<script>
+  import quickSettings from "../molecules/quickSettings";
+  import matrix from "../molecules/matrix";
+
+  export default {
+    name: "bar",
+    props: ['settings', result],
+    components: {
+      quickSettings, matrix
+    }
+  }
+</script>
+
+<style scoped>
+  .bar {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
